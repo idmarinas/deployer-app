ba940405e09629d4a09c1a701153baa974717850
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2026-05-15 12:05:10 +0200

    Confirmación inicial

---
+++ vite.config.ts
@@ -0,0 +1,49 @@
+import { defineConfig } from "vite"
+import vue from "@vitejs/plugin-vue"
+import vueDevTools from 'vite-plugin-vue-devtools'
+import ui from '@nuxt/ui/vite'
+import vueLayouts from 'vite-plugin-vue-layouts'
+import vueRouter from 'vue-router/vite'
+
+// @ts-expect-error process is a nodejs global
+const host = process.env.TAURI_DEV_HOST;
+
+// https://vite.dev/config/
+export default defineConfig(async () => ({
+  plugins: [
+    vueLayouts(),
+    vueRouter(),
+    vue(),
+    ui({
+      ui: {
+        colors: {
+          primary: 'blue',
+          neutral: 'zinc'
+        }
+      }
+    }),
+    vueDevTools(),
+  ],
+
+  // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
+  //
+  // 1. prevent Vite from obscuring rust errors
+  clearScreen: false,
+  // 2. tauri expects a fixed port, fail if that port is not available
+  server: {
+    port: 1420,
+    strictPort: true,
+    host: host || false,
+    hmr: host
+      ? {
+        protocol: "ws",
+        host,
+        port: 1421,
+      }
+      : undefined,
+    watch: {
+      // 3. tell Vite to ignore watching `src-tauri`
+      ignored: ["**/src-tauri/**"],
+    },
+  },
+}));
