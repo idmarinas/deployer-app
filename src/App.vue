6b7b1c264104307b59df41518ab47c75de76d407
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2026-05-18 17:32:29 +0200

    feat: agregar archivo App.vue con configuración inicial y gestión de rutas

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+import { computed, onBeforeMount, onMounted } from 'vue'
+import { useHead } from '@unhead/vue'
+import { useColorMode } from '@vueuse/core'
+import { useRouter } from 'vue-router'
+import { useI18n } from 'vue-i18n'
+import * as locales from '@nuxt/ui/locale'
+
+// Tauri related
+import { invoke } from '@tauri-apps/api/core'
+
+const colorMode = useColorMode()
+const router = useRouter()
+const { locale } = useI18n()
+
+const themeColor = computed(() => colorMode.value === 'dark' ? '#18181b' : '#ffffff')
+
+useHead({
+  htmlAttrs: {
+    lang: locale,
+  },
+  meta: [
+    { name: 'theme-color', content: themeColor }
+  ]
+})
+
+onBeforeMount(async () => {
+  const exists = await invoke<boolean>('check_database_exists')
+
+  if (exists) {
+    router.push('/home')
+  } else {
+    router.push('/setup')
+  }
+})
+
+onMounted(() => {
+  document.getElementById('style-splashscreen')?.remove()
+})
+</script>
+
+<template>
+  <Suspense>
+    <UApp :locale="locales[locale as keyof typeof locales]">
+      <RouterView />
+    </UApp>
+  </Suspense>
+</template>
