fe85ba36d477a46ff4484d50b953cdf84ab47fae
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2026-05-18 13:21:26 +0200

    feat: eliminar archivos de formatos de fecha y número

--- src/locales/formats.datetime.ts
+++
@@ -1,9 +0,0 @@
-import type { IntlDateTimeFormat, IntlDateTimeFormats, Locale } from 'vue-i18n'
-
-import datetimeFormatEs from './es/formats/datetime.format'
-import datetimeFormatEn from './en/formats/datetime.format'
-
-export default <IntlDateTimeFormats<Locale, IntlDateTimeFormat>>{
-    es: datetimeFormatEs,
-    en: datetimeFormatEn,
-}
--- src/locales/formats.number.ts
+++
@@ -1,9 +0,0 @@
-import type { IntlNumberFormat, IntlNumberFormats, Locale } from 'vue-i18n'
-
-import numberFormatEs from './es/formats/number.format'
-import numberFormatEn from './en/formats/number.format'
-
-export default <IntlNumberFormats<Locale, IntlNumberFormat>>{
-    es: numberFormatEs,
-    en: numberFormatEn
-}
