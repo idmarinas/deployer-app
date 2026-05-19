99ece8562db11791608d47c452d17ba54ff15c74
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2026-05-19 12:14:02 +0200

    feat: agregar layout básico centrado (vertical/horizontal) en center-center.vue

---
+++ src/layouts/center-center.vue
@@ -0,0 +1,9 @@
+<!-- Layout básico centrado (vertical/horizontal) -->
+
+<template>
+    <UMain>
+        <div class="h-screen flex items-center justify-center px-4">
+            <RouterView />
+        </div>
+    </UMain>
+</template>
