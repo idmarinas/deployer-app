9ecf5915b8055fbc4a8124f4d527bfd4cb13af39
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2026-05-21 13:49:54 +0200

    fix: elimina código innecesario y simplifica la lógica de enrutamiento en App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,12 @@
 <script setup lang="ts">
-import { computed, onBeforeMount, onMounted } from 'vue'
+import { computed, onMounted } from 'vue'
 import { useHead } from '@unhead/vue'
 import { useColorMode } from '@vueuse/core'
-import { useRouter } from 'vue-router'
 import { useI18n } from 'vue-i18n'
 import * as locales from '@nuxt/ui/locale'
 
-// Tauri related
-import { invoke } from '@tauri-apps/api/core'
-import { CommandResponse } from './types/tauri-types'
 
 const colorMode = useColorMode()
-const router = useRouter()
 const { locale } = useI18n()
 
 const themeColor = computed(() => colorMode.value === 'dark' ? '#18181b' : '#ffffff')
@@ -25,16 +20,6 @@ useHead({
   ]
 })
 
-onBeforeMount(async () => {
-  const exists = await invoke<CommandResponse<boolean>>('check_database_exists')
-
-  if (exists.success && exists.data) {
-    router.push('/')
-  } else {
-    router.push('/setup')
-  }
-})
-
 onMounted(() => {
   document.getElementById('style-splashscreen')?.remove()
 })
@@ -42,7 +27,7 @@ onMounted(() => {
 
 <template>
   <Suspense>
-    <UApp :locale="locales[locale as keyof typeof locales]" :toaster="{position: 'top-center'}">
+    <UApp :locale="locales[locale as keyof typeof locales]" :toaster="{ position: 'top-center' }">
       <RouterView />
     </UApp>
   </Suspense>
