a773be6acab63ac7f576200e425cb91b1cf31876
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2026-05-21 16:00:37 +0200

    feat: agrega lógica y componentes para gestionar migraciones en la aplicación

---
+++ src/composables/useMigrations.ts
@@ -0,0 +1,161 @@
+import type { CommandResponse } from '../types/tauri-types'
+
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+import { useI18n } from 'vue-i18n'
+import { StepperItem } from '@nuxt/ui'
+
+// Tauri related imports
+import { invoke } from '@tauri-apps/api/core'
+
+type StepStatus = 'idle' | 'loading' | 'success' | 'error'
+
+interface StepItem extends Omit<StepperItem, 'key'> {
+  id: string,
+  status: StepStatus,
+  invoke: string | Function
+}
+
+export function useMigrations() {
+  const router = useRouter()
+  const toast = useToast()
+  const { t } = useI18n()
+
+  const currentStep = ref<string | undefined>(undefined)
+  const steps = ref<StepItem[]>([])
+  const buttons = ref({
+    run: {
+      disabled: false,
+      loading: false,
+    },
+    restart: {
+      disabled: true,
+      loading: false,
+      show: false,
+    }
+  })
+
+  async function runMigrations() {
+    buttons.value.run.disabled = true
+    buttons.value.run.loading = true
+
+    steps.value = [
+      {
+        id: 'run_migrations',
+        icon: 'i-tabler-database',
+        title: t('pages.migrations.steps.title.run_migrations'),
+        description: t('pages.migrations.steps.description.idle.run_migrations'),
+        status: 'idle',
+        invoke: 'run_migrations'
+      },{
+        id: 'validate',
+        icon: 'i-tabler-database-smile',
+        title: t('pages.migrations.steps.title.validate'),
+        description: t('pages.migrations.steps.description.idle.validate'),
+        status: 'idle',
+        invoke: 'validate_sqlite_database'
+      }
+    ]
+
+    const all_steps_completed = await executeSetupSteps()
+
+    completeSetup(all_steps_completed)
+  }
+
+
+  async function executeSetupSteps(): Promise<boolean> {
+    for (const step of steps.value) {
+      setStepStatus(step.id, 'loading')
+      await new Promise(resolve => setTimeout(resolve, 1000))
+
+      let success = false
+
+      if (typeof step.invoke === 'string') {
+        const result = await invoke<CommandResponse>(step.invoke)
+        success = result.success
+
+        if (!success) {
+          toast.add({
+            title: t('components.toast.title.error'),
+            description: t(result?.message_key, result?.message_params),
+            color: 'error'
+          })
+        }
+      } else {
+        const result = await step.invoke()
+        success = result?.success ?? false
+
+        if (!success) {
+          toast.add({
+            title: t('components.toast.title.error'),
+            description: t(result?.message_key, result?.message_params),
+            color: 'error'
+          })
+        }
+      }
+
+      if (success) {
+        setStepStatus(step.id, 'success')
+      } else {
+        setStepStatus(step.id, 'error')
+        return false
+      }
+    }
+
+    return true
+  }
+
+  function setStepStatus(step: string, status: StepStatus) {
+    currentStep.value = step
+
+    const value = steps.value.find(v => v.id === step)
+
+    if (!value) return
+
+    value.status = status
+    value.description = t(`pages.migrations.steps.description.${status}.${step.replace('_pre', '').replace('_post', '')}`)
+  }
+
+  function completeSetup(all_steps_completed: boolean) {
+    if (all_steps_completed) {
+      toast.add({
+        title: t('components.toast.title.success'),
+        description: t('components.toast.description.success'),
+        color: 'success'
+      })
+
+      buttons.value.run.disabled =  true
+      buttons.value.run.loading = false
+
+      router.push('/')
+    } else {
+      buttons.value.run.disabled = true
+      buttons.value.run.loading = false
+
+      buttons.value.restart.disabled = false
+      buttons.value.restart.show = true
+    }
+
+  }
+
+  // Resetear el estado de la configuración
+  function resetMigration() {
+    buttons.value.run.disabled = false
+    buttons.value.run.loading = false
+
+    buttons.value.restart.disabled = true
+    buttons.value.restart.loading = false
+    buttons.value.restart.show = false
+
+    steps.value = []
+    currentStep.value = undefined
+  }
+
+  return {
+    currentStep,
+    steps,
+    buttons,
+    runMigrations,
+    resetMigration
+  }
+}
---
+++ src/pages/migrations.vue
@@ -0,0 +1,61 @@
+<!-- Página para gestionar migraciones -->
+
+<script setup lang="ts">
+import type { ButtonProps } from '@nuxt/ui'
+
+import { computed } from 'vue'
+
+// Composables
+import { useI18n } from 'vue-i18n'
+import { useMigrations } from '../composables/useMigrations'
+
+const { t } = useI18n()
+const { buttons, steps, currentStep, runMigrations, resetMigration } = useMigrations()
+
+definePage({
+  meta: {
+    layout: 'center-center'
+  }
+})
+
+const links = computed<ButtonProps[]>(() => {
+  const items: ButtonProps[] = [
+    {
+      label: t('pages.migrations.buttons.run'),
+      color: 'primary',
+      disabled: buttons.value.run.disabled,
+      loading: buttons.value.run.loading,
+      icon: 'i-tabler-play',
+      onClick: async () => {
+        await runMigrations()
+      }
+    }
+  ]
+
+  // Mostrar el botón de reinicio condicionalmente
+  if (buttons.value.restart.show) {
+    items.push({
+      label: t('pages.setup.buttons.restart'),
+      color: 'warning',
+      variant: 'soft',
+      icon: 'i-tabler-refresh',
+      disabled: buttons.value.restart.disabled,
+      loading: buttons.value.restart.loading,
+      onClick: () => resetMigration()
+    })
+  }
+
+  return items
+})
+
+</script>
+
+<template>
+  <UColorModeSelect class="absolute top-8 left-8 z-10" />
+  <LocaleChange class="absolute top-8 right-8 z-10" />
+
+  <UPageHero :links="links" :headline="t('pages.migrations.headline')" :title="t('pages.migrations.title')"
+    :description="t('pages.migrations.description')">
+    <UStepper :items="steps" value-key="id" :default-value="currentStep" disabled />
+  </UPageHero>
+</template>
