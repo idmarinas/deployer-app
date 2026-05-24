1199e01614e9b040f420b0a4de1607529c5fafd0
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2026-05-24 18:24:20 +0200

    fix: elimina rutas específicas en migraciones para mejorar la flexibilidad

--- src-tauri/src/commands/migrations/run_migrations.rs
+++ src-tauri/src/commands/migrations/run_migrations.rs
@@ -62,7 +62,7 @@ pub async fn run_migrations(app: AppHandle) -> CommandResponse<()> {
     };
 
     // Los archivos .sql se embeben en el binario en tiempo de compilación
-    let result = sqlx::migrate!("./migrations").run(&pool).await;
+    let result = sqlx::migrate!().run(&pool).await;
 
     pool.close().await;
     sleep_until(deadline).await;
