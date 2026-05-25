3d60dd58486c3b3201a4cf052f39420e8cc975c5
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2026-05-25 17:14:07 +0200

    refactor: actualiza importaciones para usar `useNavigationMenu` en lugar de `useDashboardToolbar`

--- src/pages/passkeys.vue
+++ src/pages/passkeys.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { useI18n } from 'vue-i18n'
-import { useDashboardToolbar } from '@/composables/useDashboardToolbar'
+import { useDashboardToolbar } from '@/composables/useNavigationMenu'
 
 const { t } = useI18n()
 const { passkeysToolbar } = useDashboardToolbar()
