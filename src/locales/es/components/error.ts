e070f0e8619886ef4259d7335863cee0aa7b7b10
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2026-05-25 16:05:21 +0200

    feat: agrega mensajes de error en español con estructura de tipo LocaleMessageValue

--- src/locales/es/components/error.ts
+++ src/locales/es/components/error.ts
@@ -1,4 +1,30 @@
-export default {
+import { LocaleMessageValue } from 'vue-i18n'
+
+export default <LocaleMessageValue> {
   statusMessage: 'No encontrado',
-  message: 'No se ha podido encontrar lo que estabas buscando.'
+  message: 'No se ha podido encontrar lo que estabas buscando.',
+  host: {
+    statusMessage: 'Servidor no encontrado',
+    message: 'No se ha podido encontrar el servidor que estabas buscando.',
+  },
+  project: {
+    statusMessage: 'Proyecto no encontrado',
+    message: 'No se ha podido encontrar el proyecto que estabas buscando.',
+  },
+  passkey: {
+    statusMessage: 'Clave de acceso no encontrada',
+    message: 'No se ha podido encontrar la clave de acceso que estabas buscando.',
+  },
+  variable: {
+    statusMessage: 'Variable no encontrada',
+    message: 'No se ha podido encontrar la variable que estabas buscando.',
+  },
+  task: {
+    statusMessage: 'Tarea no encontrada',
+    message: 'No se ha podido encontrar la tarea que estabas buscando.',
+  },
+  deployment: {
+    statusMessage: 'Despliegue no encontrado',
+    message: 'No se ha podido encontrar el despiegue que estabas buscando.'
+  }
 }
