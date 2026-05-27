04f63be88c825f39a9d42e6dfdb9c1c025084263
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2026-05-27 16:52:15 +0200

    feat: se agrega la dashboardToolbar de forma dinámica

--- src/pages/dashboard/hosts.vue
+++ src/pages/dashboard/hosts.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
 import { useI18n } from 'vue-i18n'
-import { useRoute } from 'vue-router';
+import { useRoute } from 'vue-router'
+import { useDashboardToolbarProvider } from '@/composables/useDashboardToolbar'
 
 const { t } = useI18n()
 const route = useRoute()
+
+const { toolbarContent } = useDashboardToolbarProvider('hosts')
 </script>
 
 <template>
@@ -14,16 +17,21 @@ const route = useRoute()
           <UDashboardSidebarCollapse />
         </template>
 
-      <template #right>
-        <UButton
+        <template #right>
+          <UButton
             v-if="!route.path.endsWith('/create')"
-          to="/dashboard/hosts/create"
-          icon="i-tabler-plus"
-          variant="outline"
-          :label="t('components.navigation.create.host.label')"
-        />
-      </template>
+            to="/dashboard/hosts/create"
+            icon="i-tabler-plus"
+            variant="outline"
+            :label="t('components.navigation.create.host.label')"
+          />
+        </template>
       </UDashboardNavbar>
+
+      <!-- Solo muestra el toolbar si tiene contenido -->
+      <UDashboardToolbar v-if="toolbarContent" class=" flex gap-2 justify-between">
+        <component v-for="(content, index) in toolbarContent" :key="index" :is="content" />
+      </UDashboardToolbar>
     </template>
 
     <template #body>
