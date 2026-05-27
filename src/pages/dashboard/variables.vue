f6adedd7d33501b986781d50ee04747eac306af0
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2026-05-27 16:57:20 +0200

    feat: se agrega la dashboardToolbar de forma dinámica

--- src/pages/dashboard/variables.vue
+++ src/pages/dashboard/variables.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
 import { useI18n } from 'vue-i18n'
 import { useRoute } from 'vue-router'
+import { useDashboardToolbarProvider } from '@/composables/useDashboardToolbar'
 
 const { t } = useI18n()
 const route = useRoute()
+
+const { toolbarContent } = useDashboardToolbarProvider('variables')
 </script>
 
 <template>
@@ -24,6 +27,11 @@ const route = useRoute()
           />
         </template>
       </UDashboardNavbar>
+
+      <!-- Solo muestra el toolbar si tiene contenido -->
+      <UDashboardToolbar v-if="toolbarContent" class=" flex gap-2 justify-between">
+        <component v-for="(content, index) in toolbarContent" :key="index" :is="content" />
+      </UDashboardToolbar>
     </template>
 
     <template #body>
