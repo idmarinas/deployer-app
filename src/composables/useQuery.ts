15eb99ca0e6f5058279402beff7a597888d47023
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2026-05-28 14:34:08 +0200

    feat: se añaden nuevas queries

--- src/composables/useQuery.ts
+++ src/composables/useQuery.ts
@@ -1,9 +1,20 @@
 import { useDeployerSettingsQuery } from "./queries/deployerSettings"
+import { useHostQuery } from "./queries/hosts"
+import { usePasskeysQuery } from "./queries/passkeys"
 
 export function useQuery() {
   const deployerSettingsQuery = useDeployerSettingsQuery()
+  const hostQuery = useHostQuery()
+  const passkeysQuery = usePasskeysQuery()
 
   return {
-    ...deployerSettingsQuery
+    // Deployer Settings
+    ...deployerSettingsQuery,
+
+    // Hosts
+    ...hostQuery,
+
+    // Passkeys
+    ...passkeysQuery
   }
 }
