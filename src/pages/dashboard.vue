0b393750e3e1c75a4f8be094ccf7f5896f8b5689
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2026-05-29 17:05:12 +0200

    feat: se agregan iconos sociales y el copyright

--- src/pages/dashboard.vue
+++ src/pages/dashboard.vue
@@ -5,12 +5,50 @@ import { useSideberMenu } from '@/composables/useNavigationMenu'
 
 const { navigationMenu, searchGroups } = useSideberMenu()
 
+const socials: {
+  [key: string]: {
+    icon: string
+    label: string
+    url: string
+  }
+} = {
+	x: {
+    icon: 'i-tabler-brand-x',
+    label: 'X',
+    url: 'https://x.com/idmarinas'
+  },
+	reddit: {
+    icon: 'i-tabler-brand-reddit',
+    label: 'Reddit',
+    url: 'https://reddit.com/u/idmarinas'
+  },
+	paypal: {
+    icon: 'i-tabler-brand-paypal',
+    label: 'Paypal',
+    url: 'https://www.paypal.me/idmarinas'
+  },
+	// bitly: {
+  //   icon: 'i-simple-icons-bitly',
+  //   label: 'Bitly',
+  //   url: 'https://bit.ly/m/idmarinas'
+  // },
+	github: {
+    icon: 'i-tabler-brand-github',
+    label: 'Github',
+    url: 'https://github.com/sponsors/idmarinas'
+  },
+	linkedin: {
+    icon: 'i-tabler-brand-linkedin',
+    label: 'Linkedin',
+    url: 'https://linkedin.com/in/idmarinas'
+  },
+}
 </script>
 
 <template>
   <UDashboardGroup storage="local" unit="rem">
     <UDashboardSidebar id="default" collapsible resizable :min-size="15" :default-size="20"
-      :ui="{ footer: 'border-t border-default' }">
+      :ui="{ footer: 'border-t border-default flex-col' }">
       <template #header="{ collapsed }">
         <ProjectsMenu :collapsed="collapsed" />
       </template>
@@ -23,6 +61,27 @@ const { navigationMenu, searchGroups } = useSideberMenu()
 
       <template #footer="{ collapsed }">
         <DeployerAppMenu :collapsed="collapsed" />
+        <USeparator class="mt-1 mb-4" />
+        <p  v-if="!collapsed" class="flex gap-2 items-center">
+          <UTooltip v-for="(social, label) in socials" :key="label" :text="social.label" arrow :delay-duration="300">
+            <UButton
+              :icon="social.icon"
+              variant="ghost"
+              color="neutral"
+              :to="social.url"
+              target="_blank"
+              :aria-label="social.label"
+            />
+          </UTooltip>
+        </p>
+        <p class="text-muted text-sm">
+          <span v-if="!collapsed">IDMarinas © 2026-{{ new Date().getFullYear() }}</span>
+          <span v-else>
+            <UTooltip :text="`IDMarinas © ${new Date().getFullYear()}`">
+              <UAvatar src="/logos/idmarinas.png" />
+            </UTooltip>
+          </span>
+        </p>
       </template>
     </UDashboardSidebar>
 
