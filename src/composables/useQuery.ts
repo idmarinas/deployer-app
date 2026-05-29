73a765da6e0953e324a7036e9c6ce1a3481fff83
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2026-05-29 13:42:53 +0200

    feat: se agrega query para contar "count"

--- src/composables/useQuery.ts
+++ src/composables/useQuery.ts
@@ -1,11 +1,25 @@
 import { useDeployerSettingsQuery } from "./queries/deployerSettings"
 import { useHostQuery } from "./queries/hosts"
 import { usePasskeysQuery } from "./queries/passkeys"
+import { useDatabase } from '@/composables/useDatabase'
 
 export function useQuery() {
+  const { db: database } = useDatabase()
+
   const deployerSettingsQuery = useDeployerSettingsQuery()
   const hostQuery = useHostQuery()
   const passkeysQuery = usePasskeysQuery()
+
+
+  async function count(table: string, where: string): Promise<number> {
+    return database.value!.select<{ count: number }[]>(`SELECT COUNT(*) AS count FROM ${table} WHERE ${where} LIMIT 1`)
+      .then((res) => {
+        console.log(res)
+        return res[0].count
+      })
+      .catch(() => -1)
+      .finally(() => 0)
+  }
 
   return {
     // Deployer Settings
@@ -15,6 +29,10 @@ export function useQuery() {
     ...hostQuery,
 
     // Passkeys
-    ...passkeysQuery
+    ...passkeysQuery,
+
+
+    // Common
+    count
   }
 }
