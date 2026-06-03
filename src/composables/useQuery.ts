71b1d1506163bbcd0b2437d143736ac087a54d89
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2026-06-03 19:16:41 +0200

    feat: se elimina las query de hosts

--- src/composables/useQuery.ts
+++ src/composables/useQuery.ts
@@ -1,5 +1,4 @@
 import { useDeployerSettingsQuery } from "./queries/deployerSettings"
-import { useHostQuery } from "./queries/hosts"
 import { usePasskeysQuery } from "./queries/passkeys"
 import { useDatabase } from '@/composables/useDatabase'
 
@@ -7,14 +6,12 @@ export function useQuery() {
   const { db: database } = useDatabase()
 
   const deployerSettingsQuery = useDeployerSettingsQuery()
-  const hostQuery = useHostQuery()
   const passkeysQuery = usePasskeysQuery()
 
 
   async function count(table: string, where: string): Promise<number> {
     return database.value!.select<{ count: number }[]>(`SELECT COUNT(*) AS count FROM ${table} WHERE ${where} LIMIT 1`)
       .then((res) => {
-        console.log(res)
         return res[0].count
       })
       .catch(() => -1)
@@ -24,9 +21,6 @@ export function useQuery() {
   return {
     // Deployer Settings
     ...deployerSettingsQuery,
-
-    // Hosts
-    ...hostQuery,
 
     // Passkeys
     ...passkeysQuery,
