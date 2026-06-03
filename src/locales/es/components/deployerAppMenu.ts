6c2f53982c282f3d2018b753494c391c81d68622
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2026-06-03 19:14:28 +0200

    feat: traduciones para el componente deployerAppMenu

---
+++ src/locales/es/components/deployerAppMenu.ts
@@ -0,0 +1,12 @@
+import { LocaleMessageValue } from 'vue-i18n'
+
+export default <LocaleMessageValue>{
+  settings: 'Configuración',
+  appearance: {
+    label: 'Apariencia',
+    light: 'Claro',
+    dark: 'Oscuro',
+    system: 'Sistema'
+  },
+  locale: 'Idioma',
+}
