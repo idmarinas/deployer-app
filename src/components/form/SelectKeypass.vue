ce4d3cbae4ca80ee6e2d4d78b98dc5b42b7f5d50
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2026-06-04 15:20:23 +0200

    feat: se usa el loader de pinia colada

--- src/components/form/SelectKeypass.vue
+++ src/components/form/SelectKeypass.vue
@@ -1,21 +1,13 @@
-<script setup lang="ts">
+<script lang="ts">
 import type { SelectMenuItem } from '@nuxt/ui'
 
-import { onMounted } from 'vue'
 import { useI18n } from 'vue-i18n'
+import { useSelectPasskeys } from '@/loaders/passkeys'
+</script>
 
-import { usePasskeysQuery } from '@/composables/queries/passkeys'
-import { useQueryState } from '@/composables/useQueryState'
-
+<script setup lang="ts">
 const { t } = useI18n()
-const { populateSelectOptions } = usePasskeysQuery()
-
-const { data: items, loading: isLoading, execute } = useQueryState(populateSelectOptions)
-
-onMounted(() => {
-  execute()
-})
-
+const { data: items, isLoading } = useSelectPasskeys()
 </script>
 
 <template>
