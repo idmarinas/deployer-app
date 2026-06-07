763019b199795e284223cd0013b1045f41533340
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2026-06-07 18:56:23 +0200

    feat: se cambia el nombre

--- src/composables/useDialog.ts
+++ src/composables/useDialog.ts
@@ -1,4 +1,5 @@
 import ConfirmDialog from '@/components/overlay/ConfirmDialog.vue'
+import Formdialog from '@/components/overlay/Formdialog.vue'
 
 export interface ConfirmDialogOptions {
   title: string
@@ -6,11 +7,29 @@ export interface ConfirmDialogOptions {
   type: 'yes_no' | 'cancel_confirm' | 'cancel_delete'
 }
 
+export interface FormDialogOptions {
+  title: string
+  description?: string
+}
+
 export const useConfirmDialog = () => {
   const overlay = useOverlay()
 
   return (options: ConfirmDialogOptions): Promise<boolean> => {
     const modal = overlay.create(ConfirmDialog, {
+      destroyOnClose: true,
+      props: options
+    })
+
+    return modal.open()
+  }
+}
+
+export const useFormDialog = () => {
+  const overlay = useOverlay()
+
+  return (options: FormDialogOptions): Promise<string> => {
+    const modal = overlay.create(Formdialog, {
       destroyOnClose: true,
       props: options
     })
