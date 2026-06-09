28840bb07af466c7f6f9f03cfd4327765a5d3e24
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2026-06-09 19:43:35 +0200

    feat: se agrega el dialogo para copiar una clave a un servidor

---
+++ src/components/overlay/forms/CopyPasskeyToServerDialog.vue
@@ -0,0 +1,114 @@
+<script lang="ts">
+import type { CommandResponse, ExportPublicKeyInput, Passkey } from '@/types/tauri-types'
+import type { FormSubmitEvent } from '@nuxt/ui'
+
+import { useTemplateRef, ref } from 'vue'
+import { useI18n } from 'vue-i18n'
+
+import { invoke } from '@tauri-apps/api/core'
+
+import { usePasskeySchema } from '@/composables/schemas/passkeys'
+</script>
+
+<script setup lang="ts">
+const emits = defineEmits<{
+  close: [value: boolean]
+}>()
+const props = defineProps<{
+  passkey: Passkey
+}>()
+
+const { passkeyToServerSchema } = usePasskeySchema()
+const { t } = useI18n()
+const toast = useToast()
+
+const isLoading = ref(false)
+
+const state = ref<Partial<ExportPublicKeyInput>>({
+  passkey_id: props.passkey.id,
+  host_id: undefined,
+  action: undefined,
+  temp_password: undefined,
+  temp_username: undefined
+})
+
+
+const form = useTemplateRef('form-copy-passkey')
+
+async function onSubmit(event: FormSubmitEvent<Partial<ExportPublicKeyInput>>) {
+  isLoading.value = true
+  const input = event.data
+
+  const response = await invoke<CommandResponse>('export_public_key', { input })
+  let result = true
+
+  if (response.success) {
+    toast.add({
+      title: t('overlays.toast.title.success'),
+      description: t(response.message_key, response.message_params),
+      color: 'success'
+    })
+  } else {
+    result = false
+    toast.add({
+      title: t('overlays.toast.title.error'),
+      description: t(response.message_key, response.message_params),
+      color: 'error'
+    })
+  }
+
+  emits('close', result)
+  isLoading.value = false
+}
+</script>
+
+<template>
+    <UModal
+      :title="t('schemas.passkeys.copy_to_host.title')"
+      :description="t('schemas.passkeys.copy_to_host.description', { name: props.passkey.name})"
+      :close="false"
+      :dismissible="false"
+      :ui="{ content: 'max-w-xl' }"
+    >
+      <template #body>
+        <UForm ref="form-copy-passkey" :state="state as any" :schema="passkeyToServerSchema" :disabled="isLoading" class="flex flex-col gap-4" @submit="onSubmit">
+          <UFormField name="host_id" :label="t('schemas.passkeys.form.server.label')" :help="t('schemas.passkeys.form.server.help')" required>
+            <SelectHost v-model="state.host_id" class="w-full" />
+          </UFormField>
+        </UForm>
+      </template>
+      <template #footer>
+        <UButton
+          :label="t(`overlays.dialog.passkey_to_server.remove`)"
+          :loading="isLoading"
+          icon="i-tabler-trash"
+          color="error"
+          variant="outline"
+          @click="() => {
+            state.action = 'remove'
+            form?.submit()
+          }"
+        />
+        <div class="flex-1"></div>
+        <UButton
+          :label="t(`overlays.dialog.passkey_to_server.copy`)"
+          :loading="isLoading"
+          icon="i-tabler-plus"
+          color="primary"
+          variant="outline"
+          @click="() => {
+            state.action = 'add'
+            form?.submit()
+          }"
+        />
+        <UButton
+          :label="t(`overlays.dialog.passkey_to_server.cancel`)"
+          :loading="isLoading"
+          icon="i-tabler-x"
+          color="neutral"
+          variant="outline"
+          @click="emits('close', false)"
+        />
+      </template>
+    </UModal>
+</template>
