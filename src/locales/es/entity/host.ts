4fed38a086f5ab1fb0eabf2de2136e8f41105f62
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2026-06-09 19:44:31 +0200

    feat: create host.ts and passkey.ts

---
+++ src/locales/es/entity/host.ts
@@ -0,0 +1,16 @@
+import { LocaleMessageValue } from "vue-i18n"
+
+export default <LocaleMessageValue>{
+  label: 'Servidor',
+  name: 'Nombre',
+  description: 'Descripción',
+  host: 'Servidor',
+  port: 'Puerto',
+  auth_type: 'Tipo de autenticación',
+  username: 'Usuario',
+  password: 'Contraseña',
+  key_id: 'ID de clave',
+  enabled: 'Habilitado',
+  created_at: 'Fecha de creación',
+  updated_at: 'Fecha de actualización'
+}
---
+++ src/locales/es/entity/passkey.ts
@@ -0,0 +1,13 @@
+import { LocaleMessageValue } from "vue-i18n"
+
+export default <LocaleMessageValue>{
+  name: 'Nombre',
+  description: 'Descripción',
+  key_type: 'Tipo de clave',
+  key_content: 'Contenido de la clave',
+  key_public: 'Clave pública',
+  passphrase: 'Frase de contraseña',
+  fingerprint: 'Huella digital',
+  created_at: 'Fecha de creación',
+  updated_at: 'Fecha de actualización'
+}
