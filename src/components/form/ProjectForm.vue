a45dd3ffe0e02e0c329ab8a9ac4867a6c4985bbf
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2026-06-17 14:11:06 +0200

    feat: formulario para proyectos

---
+++ src/components/form/ProjectForm.vue
@@ -0,0 +1,137 @@
+<script lang="ts">
+import { useI18n } from 'vue-i18n'
+
+import { useProjectSchema } from '@/composables/schemas/projects'
+
+import { open } from '@tauri-apps/plugin-dialog'
+</script>
+
+<script setup lang="ts">
+const state = defineModel<{
+	name: string
+	description?: string
+	git_url: string
+	framework: string
+	local_working_dir: string
+	remote_working_dir: string
+}>({ required: true })
+
+const { t } = useI18n()
+
+const { projectSchema: schema } = useProjectSchema()
+</script>
+
+<template>
+	<UFormField
+		name="name"
+		:label="t('schemas.passkeys.form.name.label')"
+		:help="t('schemas.passkeys.form.name.help')"
+		required
+	>
+		<UInput
+			v-model="state.name"
+			autocomplete="off"
+			class="w-full"
+			:ui="{ trailing: 'pointer-events-none' }"
+			:maxlength="schema.shape.name.maxLength || undefined"
+		>
+			<template #trailing>
+				<div id="character-count" class="text-xs text-muted tabular-nums" aria-live="polite" role="status">
+					{{ state.name?.length ?? 0 }}/{{ schema.shape.name.maxLength || 0 }}
+				</div>
+			</template>
+		</UInput>
+	</UFormField>
+
+	<UFormField
+		name="description"
+		:label="t('schemas.passkeys.form.description.label')"
+		:help="t('schemas.passkeys.form.description.help')"
+		:hint="t('schemas.form.hint.optional')"
+	>
+		<UTextarea
+			v-model="state.description"
+			class="w-full"
+			:ui="{ trailing: 'pointer-events-none' }"
+			:maxlength="schema.shape.description.def.innerType.maxLength || undefined"
+		>
+			<template #trailing>
+				<div id="character-count" class="text-xs text-muted tabular-nums" aria-live="polite" role="status">
+					{{ state.description?.length ?? 0 }}/{{ schema.shape.description.def.innerType.maxLength || 0 }}
+				</div>
+			</template>
+		</UTextarea>
+	</UFormField>
+
+	<UFormField
+		name="git_url"
+		:label="t('schemas.projects.form.git_url.label')"
+		:description="t('schemas.projects.form.git_url.description')"
+		:help="t('schemas.projects.form.git_url.help')"
+		required
+	>
+		<UInput type="url" v-model="state.git_url" class="w-full" autocomplete="off" />
+	</UFormField>
+	<UFormField
+		name="framework"
+		:label="t('schemas.projects.form.framework.label')"
+		:help="t('schemas.projects.form.framework.help')"
+		required
+	>
+		<USelect
+			v-model="state.framework"
+			:items="[
+				{
+					label: t('schemas.projects.form.framework.select.symfony.label'),
+					description: t('schemas.projects.form.framework.select.symfony.description'),
+					value: 'symfony',
+				},
+				{
+					label: t('schemas.projects.form.framework.select.laravel.label'),
+					description: t('schemas.projects.form.framework.select.laravel.description'),
+					value: 'laravel',
+				},
+				{
+					label: t('schemas.projects.form.framework.select.nextjs.label'),
+					description: t('schemas.projects.form.framework.select.nextjs.description'),
+					value: 'nextjs',
+				},
+				{
+					label: t('schemas.projects.form.framework.select.generic.label'),
+					description: t('schemas.projects.form.framework.select.generic.description'),
+					value: 'generic',
+				},
+			]"
+			value-key="value"
+			class="w-full"
+		/>
+	</UFormField>
+
+	<UFormField
+		name="local_working_dir"
+		:label="t('schemas.projects.form.local_working_dir.label')"
+		:help="t('schemas.projects.form.local_working_dir.help')"
+		required
+	>
+		<UFieldGroup class="w-full">
+			<UInput v-model="state.local_working_dir" class="w-full" autocomplete="off" />
+			<UButton
+				icon="i-tabler-folder"
+				@click="
+					async () => {
+						state.local_working_dir = (await open({ multiple: false, directory: true })) || ''
+					}
+				"
+			/>
+		</UFieldGroup>
+	</UFormField>
+
+	<UFormField
+		name="remote_working_dir"
+		:label="t('schemas.projects.form.remote_working_dir.label')"
+		:help="t('schemas.projects.form.remote_working_dir.help')"
+		required
+	>
+		<UInput v-model="state.remote_working_dir" class="w-full" autocomplete="off" />
+	</UFormField>
+</template>
