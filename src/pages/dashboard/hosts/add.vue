813247c5789f555f5870ce1dca4c3e096c5a2096
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2026-06-18 20:19:29 +0200

    chore: se adapta para los cambios en useToolbarContent

--- src/pages/dashboard/hosts/add.vue
+++ src/pages/dashboard/hosts/add.vue
@@ -1,12 +1,17 @@
 <script setup lang="ts">
-import type { FormSubmitEvent } from '@nuxt/ui'
+import type { FormSubmitEvent, Form } from '@nuxt/ui'
 import type { CommandResponse, CreateHostInput } from '@/types/tauri-types'
 
 import { ref, useTemplateRef, onMounted, onBeforeUnmount, watch } from 'vue'
 
 import { useI18n } from 'vue-i18n'
 import { useDashboardToolbar } from '@/composables/useDashboardToolbar'
-import { useHostSchema, type HostSchema, type AuthKeySchema, type AuthPasswordSchema } from '@/composables/schemas/hosts'
+import {
+	useHostSchema,
+	type HostSchema,
+	type AuthKeySchema,
+	type AuthPasswordSchema,
+} from '@/composables/schemas/hosts'
 import { useToolbarContentCreate } from '@/composables/useToolbarContent'
 import { useToast } from '@nuxt/ui/composables/useToast'
 import { useRouter } from 'vue-router'
@@ -14,7 +19,7 @@ import { useRouter } from 'vue-router'
 import { invoke } from '@tauri-apps/api/core'
 
 definePage({
-  name: 'dashboard-hosts-add'
+	name: 'dashboard-hosts-add',
 })
 
 const { t } = useI18n()
@@ -27,61 +32,67 @@ const { hostSchema } = useHostSchema()
 type HostFullSchema = HostSchema & (AuthPasswordSchema | AuthKeySchema)
 
 const initialState: HostFullSchema = {
-  name: '',
-  description: undefined,
-  host: '',
-  port: 22,
-  auth_type: 'password',
-  username: '',
-  password: '',
-  key_id: null,
-  enabled: false
+	name: '',
+	description: undefined,
+	host: '',
+	port: 22,
+	auth_type: 'password',
+	username: '',
+	password: '',
+	key_id: null,
+	enabled: false,
 }
-const state = ref<any>({...initialState})
-const form = useTemplateRef('form')
+const state = ref<any>({ ...initialState })
+const form = useTemplateRef<Form<HostSchema>>('form')
 const isLoading = ref(false)
 
-const handleReset = () => {
-  Object.assign(state, initialState)
-  form.value?.clear()
-}
-
-const updateToolbar = () => toolbar?.setToolbarContent(generateToolbarContent())
 // Generar contenido del toolbar
-const generateToolbarContent = useToolbarContentCreate('hosts', state, isLoading, updateToolbar, () => form.value?.submit(), handleReset)
+useToolbarContentCreate(state, initialState, isLoading, form, toolbar)
 
-async function onSubmit(event: FormSubmitEvent<HostSchema>){
-  isLoading.value = true
-  const host: Partial<CreateHostInput> = event.data
+async function onSubmit(event: FormSubmitEvent<HostSchema>) {
+	isLoading.value = true
+	const host: Partial<CreateHostInput> = event.data
 
-  const result = await invoke<CommandResponse<number>>('crud_create_host', {input: host})
+	const result = await invoke<CommandResponse<number>>('crud_create_host', { input: host })
 
-  if (result.success) {
-    toast.add({title: t('overlays.toast.title.success'), description: t('schemas.hosts.added', { name: host.name }), color: 'success'})
-    isLoading.value = false
-    router.push({ name: 'dashboard-hosts' })
-  } else {
-    toast.add({title: t('overlays.toast.title.error'), description: result.message_key, color: 'error'})
-    isLoading.value = false
-  }
+	if (result.success) {
+		toast.add({
+			title: t('overlays.toast.title.success'),
+			description: t('schemas.hosts.added', { name: host.name }),
+			color: 'success',
+		})
+		isLoading.value = false
+		router.push({ name: 'dashboard-hosts' })
+	} else {
+		toast.add({ title: t('overlays.toast.title.error'), description: result.message_key, color: 'error' })
+		isLoading.value = false
+	}
 }
 
 // Inyectar contenido en el toolbar cuando se monta el componente
 onMounted(() => {
-  updateToolbar()
+	toolbar?.updateToolbar()
 })
 
 // Limpiar el toolbar cuando se desmonta
 onBeforeUnmount(() => {
-  toolbar?.clearToolbarContent()
+	toolbar?.clearContent()
 })
 
 // Actualizar toolbar cuando isLoading cambia
-watch(isLoading, updateToolbar)
+watch(isLoading, () => toolbar?.updateToolbar())
 </script>
 
 <template>
-  <UForm ref="form" :disabled="isLoading" id="form-host-create" :schema="hostSchema" :state="state" class="grid grid-cols-1 md:grid-cols-2 gap-4" @submit="onSubmit">
-    <HostForm v-model="state" :is-loading="isLoading" />
-  </UForm>
+	<UForm
+		ref="form"
+		:disabled="isLoading"
+		id="form-host-create"
+		:schema="hostSchema"
+		:state="state"
+		class="grid grid-cols-1 md:grid-cols-2 gap-4"
+		@submit="onSubmit"
+	>
+		<HostForm v-model="state" :is-loading="isLoading" />
+	</UForm>
 </template>
