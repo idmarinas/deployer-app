5a4d77cc2b892d47060ad721e555f391402239be
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2026-06-22 17:03:09 +0200

    chore: se agrega deploymentHistory

--- src/composables/useQuery.ts
+++ src/composables/useQuery.ts
@@ -1,26 +1,31 @@
-import { useDeployerSettingsQuery } from "./queries/deployerSettings"
 import { useDatabase } from '@/composables/useDatabase'
 
+import { useDeployerSettingsQuery } from './queries/deployerSettings'
+import { useDeploymentHistoryQuery } from './queries/deploymentHistory'
+
 export function useQuery() {
-  const { db: database } = useDatabase()
+	const { db: database } = useDatabase()
 
-  const deployerSettingsQuery = useDeployerSettingsQuery()
+	const deployerSettingsQuery = useDeployerSettingsQuery()
+	const deploymentHistoryQuery = useDeploymentHistoryQuery()
 
+	async function count(table: string, where: string): Promise<number> {
+		return database
+			.value!.select<{ count: number }[]>(`SELECT COUNT(*) AS count FROM ${table} WHERE ${where} LIMIT 1`)
+			.then(res => {
+				return res[0].count
+			})
+			.catch(() => -1)
+			.finally(() => 0)
+	}
 
-  async function count(table: string, where: string): Promise<number> {
-    return database.value!.select<{ count: number }[]>(`SELECT COUNT(*) AS count FROM ${table} WHERE ${where} LIMIT 1`)
-      .then((res) => {
-        return res[0].count
-      })
-      .catch(() => -1)
-      .finally(() => 0)
-  }
+	return {
+		// Deployer Settings
+		...deployerSettingsQuery,
+		// Deployment History
+		...deploymentHistoryQuery,
 
-  return {
-    // Deployer Settings
-    ...deployerSettingsQuery,
-
-    // Common
-    count
-  }
+		// Common
+		count,
+	}
 }
