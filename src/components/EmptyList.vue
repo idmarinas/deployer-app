15c623aabbf4f9746c5f56c006c24ffc251d7445
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2026-06-29 15:57:24 +0200

    chore: componente para mostrar que la lista está vacia, con botón de añadir y de actualizar

---
+++ src/components/EmptyList.vue
@@ -0,0 +1,37 @@
+<script lang="ts">
+import { getModuleIcon, ICONS } from '@/utils/icons'
+import { useI18n } from 'vue-i18n'
+import { RouteLocationRaw } from 'vue-router'
+</script>
+
+<script setup lang="ts">
+const { t } = useI18n()
+
+const props = defineProps<{
+	module: 'projects' | 'tasks' | 'passkeys' | 'hosts' | 'variables'
+	reloadFn: () => void
+	addRoute?: RouteLocationRaw
+}>()
+</script>
+
+<template>
+	<UEmpty
+		:icon="getModuleIcon(props.module)"
+		:title="t(`pages.${props.module}.table.empty.title`)"
+		:description="t(`pages.${props.module}.table.empty.description`)"
+		:actions="[
+			{
+				icon: ICONS.actions.add,
+				label: t(`components.buttons.${props.module}.add.label`),
+				to: props.addRoute,
+			},
+			{
+				icon: ICONS.actions.refresh,
+				label: t('common.actions.refresh'),
+				color: 'neutral',
+				variant: 'soft',
+				onClick: () => reloadFn(),
+			},
+		]"
+	/>
+</template>
