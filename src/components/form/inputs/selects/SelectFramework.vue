60f43486c64cb6184018680588118472fdfc252d
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2026-07-01 18:16:11 +0200

    feat: se agrega select para los frameworks

---
+++ src/components/form/inputs/selects/SelectFramework.vue
@@ -0,0 +1,39 @@
+<script lang="ts">
+import { useI18n } from 'vue-i18n'
+</script>
+
+<script setup lang="ts">
+const { t } = useI18n()
+
+const items = [
+	{
+		label: t('form.shared.select.framework.symfony.label'),
+		description: t('form.shared.select.framework.symfony.description'),
+		value: 'symfony',
+	},
+	// {
+	// 	label: t('form.shared.select.framework.laravel.label'),
+	// 	description: t('form.shared.select.framework.laravel.description'),
+	// 	value: 'laravel',
+	// },
+	// {
+	// 	label: t('form.shared.select.framework.nextjs.label'),
+	// 	description: t('form.shared.select.framework.nextjs.description'),
+	// 	value: 'nextjs',
+	// },
+	{
+		label: t('form.shared.select.framework.vuejs.label'),
+		description: t('form.shared.select.framework.vuejs.description'),
+		value: 'vuejs',
+	},
+	{
+		label: t('form.shared.select.framework.generic.label'),
+		description: t('form.shared.select.framework.generic.description'),
+		value: 'generic',
+	},
+]
+</script>
+
+<template>
+	<USelectMenu value-key="value" :placeholder="t('form.shared.placeholder.framework.select')" :items="items" />
+</template>
