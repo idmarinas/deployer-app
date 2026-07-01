46f6b2917e1db28d480fbdaacb4169974cc37fb5
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2026-07-01 19:17:28 +0200

    chore: se agrega poder eliminar ciertos ids de hosts

--- src/components/form/inputs/selects/SelectHost.vue
+++ src/components/form/inputs/selects/SelectHost.vue
@@ -1,36 +1,51 @@
 <script lang="ts">
 import type { SelectMenuItem } from '@nuxt/ui'
 
-import { useI18n } from 'vue-i18n'
 import { useHostSelectPopulate } from '@/loaders/hosts'
+import { computed } from 'vue'
+import { useI18n } from 'vue-i18n'
 
 type SelectMenuItemExtends = SelectMenuItem & {
-  id: number
-  username: string
-  enabled: boolean
+	id: number
+	username: string
+	enabled: boolean
 }
 </script>
 
 <script setup lang="ts">
+const props = defineProps<{
+	ignoreHosts?: Set<number>
+}>()
+
 const { t } = useI18n()
 const { data: items, isLoading } = useHostSelectPopulate()
+
+const availableItems = computed(() => {
+	const ignoreHosts = props.ignoreHosts || new Set([])
+	return ((items.value ?? []) as any[]).filter(host => !ignoreHosts.has(host.id))
+})
 </script>
 
 <template>
-  <USelectMenu
-    clear
-    value-key="id"
-    :items="items as SelectMenuItemExtends[]"
-    :loading="isLoading"
-    :disabled="isLoading"
-    :placeholder="t('form.shared.placeholder.hosts.select')"
-    icon="i-tabler-server"
-  >
-    <template #item-leading="{ item }">
-      <UBadge :color="item.enabled ? 'success' : 'error'" variant="outline" :icon="item.enabled ? 'i-tabler-server' : 'i-tabler-server-off'" size="sm" />
-    </template>
-    <template #item-trailing="{ item }">
-      <UBadge color="neutral" variant="outline" icon="i-tabler-user" size="sm">{{ item.username }}</UBadge>
-    </template>
-  </USelectMenu>
+	<USelectMenu
+		clear
+		value-key="id"
+		:items="availableItems as SelectMenuItemExtends[]"
+		:loading="isLoading"
+		:disabled="isLoading"
+		:placeholder="t('form.shared.placeholder.hosts.select')"
+		icon="i-tabler-server"
+	>
+		<template #item-leading="{ item }">
+			<UBadge
+				:color="item.enabled ? 'success' : 'error'"
+				variant="outline"
+				:icon="item.enabled ? 'i-tabler-server' : 'i-tabler-server-off'"
+				size="sm"
+			/>
+		</template>
+		<template #item-trailing="{ item }">
+			<UBadge color="neutral" variant="outline" icon="i-tabler-user" size="sm">{{ item.username }}</UBadge>
+		</template>
+	</USelectMenu>
 </template>
