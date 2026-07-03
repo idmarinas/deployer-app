47a8b6968ed797e5dc2021d0b1b20866cf77efeb
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2026-07-03 14:16:30 +0200

    fix: se corrige error en la clave de traducción

--- src/components/table/GlobalFilter.vue
+++ src/components/table/GlobalFilter.vue
@@ -8,9 +8,19 @@ const { t } = useI18n()
 </script>
 
 <template>
-  <UInput v-model="globalFilter" :placeholder="t('common.filter_ellipsis')" :ui="{ trailing: 'pe-1' }">
-    <template v-if="globalFilter?.length" #trailing>
-      <UButton color="neutral" variant="link" icon="i-tabler-circle-x" aria-label="Clear input" @click="globalFilter = ''" />
-    </template>
-  </UInput>
+	<UInput v-model="globalFilter" :placeholder="t('common.actions.filter')" :ui="{ trailing: 'pe-1' }">
+		<template v-if="globalFilter?.length" #trailing>
+			<UButton
+				color="neutral"
+				variant="link"
+				icon="i-tabler-circle-x"
+				aria-label="Clear input"
+				@click="
+					() => {
+						globalFilter = ''
+					}
+				"
+			/>
+		</template>
+	</UInput>
 </template>
