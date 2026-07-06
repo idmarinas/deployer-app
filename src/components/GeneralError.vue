c5c5a785e5414ee4d0f0cc348889fe0e1acef77f
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2026-07-06 18:40:40 +0200

    chore: nuevo componente

---
+++ src/components/GeneralError.vue
@@ -0,0 +1,19 @@
+<script lang="ts">
+import { useI18n } from 'vue-i18n'
+</script>
+
+<script setup lang="ts">
+const { t } = useI18n()
+</script>
+
+<template>
+	<UError
+		icon="i-tabler-exclamation-circle"
+		:clear="{ to: { name: 'dashboard-home' } }"
+		:ui="{ root: 'h-full min-h-full' }"
+		:error="{
+			statusMessage: t(`components.error.unknown.statusMessage`),
+			message: t(`components.error.unknown.message`),
+		}"
+	/>
+</template>
