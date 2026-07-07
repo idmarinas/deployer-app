fdd19cf785a98df4079a94095af964910cb52ec6
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2026-07-07 19:49:57 +0200

    fix: se agrega la ruta correcta a la configuración

--- src/components/DeployerAppMenu.vue
+++ src/components/DeployerAppMenu.vue
@@ -1,12 +1,12 @@
 <script lang="ts">
-import { computed } from 'vue'
 import type { DropdownMenuItem } from '@nuxt/ui'
+import { computed } from 'vue'
 
 import { getCurrentWindow, LogicalSize } from '@tauri-apps/api/window'
 
-import { useI18n } from 'vue-i18n'
-import { useColorMode } from '@vueuse/core'
 import * as uiLocales from '@nuxt/ui/locale'
+import { useColorMode } from '@vueuse/core'
+import { useI18n } from 'vue-i18n'
 
 import { useLocale } from '@/composables/useLocale'
 import { ICONS } from '@/utils/icons'
@@ -14,7 +14,7 @@ import { ICONS } from '@/utils/icons'
 
 <script setup lang="ts">
 defineProps<{
-  collapsed?: boolean
+	collapsed?: boolean
 }>()
 
 const { locale, setLocale, availableLocales, isLoading: isLocaleLoading } = useLocale()
@@ -23,92 +23,121 @@ const { t } = useI18n()
 const appConfig = useAppConfig()
 const colorMode = useColorMode()
 
-const items = computed<DropdownMenuItem[][]>(() => ([[{
-  type: 'label',
-  label: t('app.title'),
-  avatar: {
-    icon: ICONS.app.logo,
-    src: '/logo.png',
-    class: 'bg-transparent',
-    alt: 'DeployerApp Logo',
-    size: 'lg',
-    ui: {
-      root: 'rounded-none'
-    }
-  }
-}], [{
-  label: t('components.deployerAppMenu.settings'),
-  icon: ICONS.app.settings,
-  to: '/app/settings'
-},{
-    label: 'Restablecer ventana',
-    icon: ICONS.app.window,
-    onClick: async () => {
-      const win = getCurrentWindow()
-      await win.setSize(new LogicalSize(1400, 900))
-      await win.center()
-    }
-  }
-], [{
-  label: t('components.deployerAppMenu.appearance.label'),
-  icon: ICONS.app.appearance,
-  children: [{
-    label: t('components.deployerAppMenu.appearance.light'),
-    icon: appConfig.ui.icons.light,
-    type: 'checkbox',
-    checked: colorMode.value === 'light',
-    onSelect(e: Event) {
-      e.preventDefault()
+const items = computed<DropdownMenuItem[][]>(() => [
+	[
+		{
+			type: 'label',
+			label: t('app.title'),
+			avatar: {
+				icon: ICONS.app.logo,
+				src: '/logo.png',
+				class: 'bg-transparent',
+				alt: 'DeployerApp Logo',
+				size: 'lg',
+				ui: {
+					root: 'rounded-none',
+				},
+			},
+		},
+	],
+	[
+		{
+			label: t('components.deployerAppMenu.settings'),
+			icon: ICONS.app.settings,
+			to: { name: 'dashboard-app-settings' },
+		},
+		{
+			label: 'Restablecer ventana',
+			icon: ICONS.app.window,
+			onClick: async () => {
+				const win = getCurrentWindow()
+				await win.setSize(new LogicalSize(1400, 900))
+				await win.center()
+			},
+		},
+	],
+	[
+		{
+			label: t('components.deployerAppMenu.appearance.label'),
+			icon: ICONS.app.appearance,
+			children: [
+				{
+					label: t('components.deployerAppMenu.appearance.light'),
+					icon: appConfig.ui.icons.light,
+					type: 'checkbox',
+					checked: colorMode.value === 'light',
+					onSelect(e: Event) {
+						e.preventDefault()
 
-      colorMode.value = 'light'
-    }
-  }, {
-    label: t('components.deployerAppMenu.appearance.dark'),
-    icon: appConfig.ui.icons.dark,
-    type: 'checkbox',
-    checked: colorMode.value === 'dark',
-    onSelect(e: Event) {
-      e.preventDefault()
+						colorMode.value = 'light'
+					},
+				},
+				{
+					label: t('components.deployerAppMenu.appearance.dark'),
+					icon: appConfig.ui.icons.dark,
+					type: 'checkbox',
+					checked: colorMode.value === 'dark',
+					onSelect(e: Event) {
+						e.preventDefault()
 
-      colorMode.value = 'dark'
-    }
-  }]
-}, {
-  label: t('components.deployerAppMenu.locale'),
-  icon: ICONS.app.language,
-  children: Object.values(uiLocales).filter(lang => availableLocales.includes(lang.code)).map(lang => ({
-    label: lang.name,
-    icon: `circle-flags:lang-${lang.code}`,
-    type: 'checkbox',
-    name: 'locale',
-    loading: isLocaleLoading.value,
-    disabled: isLocaleLoading.value,
-    checked: locale.value === lang.code,
-    onSelect(e: Event) {
-      e.preventDefault()
-      setLocale(lang.code)
-    }
-  }))
-}]
-]))
+						colorMode.value = 'dark'
+					},
+				},
+			],
+		},
+		{
+			label: t('components.deployerAppMenu.locale'),
+			icon: ICONS.app.language,
+			children: Object.values(uiLocales)
+				.filter(lang => availableLocales.includes(lang.code))
+				.map(lang => ({
+					label: lang.name,
+					icon: `circle-flags:lang-${lang.code}`,
+					type: 'checkbox',
+					name: 'locale',
+					loading: isLocaleLoading.value,
+					disabled: isLocaleLoading.value,
+					checked: locale.value === lang.code,
+					onSelect(e: Event) {
+						e.preventDefault()
+						setLocale(lang.code)
+					},
+				})),
+		},
+	],
+])
 </script>
 
 <template>
-  <UDropdownMenu :items="items" :content="{ align: 'center', collisionPadding: 12 }"
-    :ui="{ content: collapsed ? 'w-48' : 'w-(--reka-dropdown-menu-trigger-width)' }">
-    <UButton :avatar="{ icon: ICONS.app.logo, src: '/logo.png', class: 'bg-transparent', ui: {root: 'rounded-none'}}" :label="collapsed ? undefined : t('app.title')"
-      :trailing-icon="collapsed ? undefined : 'i-tabler-selector'" color="neutral" variant="ghost" block
-      :square="collapsed" class="data-[state=open]:bg-elevated" :ui="{
-        trailingIcon: 'text-dimmed'
-      }" />
+	<UDropdownMenu
+		:items="items"
+		:content="{ align: 'center', collisionPadding: 12 }"
+		:ui="{ content: collapsed ? 'w-48' : 'w-(--reka-dropdown-menu-trigger-width)' }"
+	>
+		<UButton
+			:avatar="{ icon: ICONS.app.logo, src: '/logo.png', class: 'bg-transparent', ui: { root: 'rounded-none' } }"
+			:label="collapsed ? undefined : t('app.title')"
+			:trailing-icon="collapsed ? undefined : 'i-tabler-selector'"
+			color="neutral"
+			variant="ghost"
+			block
+			:square="collapsed"
+			class="data-[state=open]:bg-elevated"
+			:ui="{
+				trailingIcon: 'text-dimmed',
+			}"
+		/>
 
-    <template #chip-leading="{ item }">
-      <div class="inline-flex items-center justify-center shrink-0 size-5">
-        <span class="rounded-full ring ring-bg bg-(--chip-light) dark:bg-(--chip-dark) size-2" :style="{
-          '--chip-light': `var(--color-${(item as any).chip}-500)`,
-          '--chip-dark': `var(--color-${(item as any).chip}-400)`
-        }" />
-      </div>
-    </template>
-  </UDropdownMenu>
+		<template #chip-leading="{ item }">
+			<div class="inline-flex items-center justify-center shrink-0 size-5">
+				<span
+					class="rounded-full ring ring-bg bg-(--chip-light) dark:bg-(--chip-dark) size-2"
+					:style="{
+						'--chip-light': `var(--color-${(item as any).chip}-500)`,
+						'--chip-dark': `var(--color-${(item as any).chip}-400)`,
+					}"
+				/>
+			</div>
+		</template>
+	</UDropdownMenu>
 </template>
