1518e0dcbfcd044b7efc68a324dd621197604500
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2026-07-09 19:36:00 +0200

    chore: se elimina el crud de la tabla encryption_config

--- src-tauri/src/commands/encryption_config/mod.rs
+++
@@ -1,8 +0,0 @@
-pub mod list_encryption_config;
-pub mod migrate_encryption_field;
-pub mod types;
-pub mod update_encryption_config;
-
-pub use list_encryption_config::list_encryption_config;
-pub use migrate_encryption_field::migrate_encryption_field;
-pub use update_encryption_config::update_encryption_config;
