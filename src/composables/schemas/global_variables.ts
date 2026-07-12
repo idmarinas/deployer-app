1e7076071ae4f2f6194e271111d5744a0a7d5e90
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2026-07-12 18:43:38 +0200

    chore: se agrega la validación para slug

--- src/composables/schemas/global_variables.ts
+++ src/composables/schemas/global_variables.ts
@@ -1,33 +1,40 @@
-import { and, eq, ne } from 'drizzle-orm'
-import * as z from 'zod'
-import { useI18n } from 'vue-i18n'
 import { countWhere } from '@/composables/queries/shared'
 import { global_variables } from '@/lib/schema'
+import { and, eq, ne } from 'drizzle-orm'
+import { useI18n } from 'vue-i18n'
+import * as z from 'zod'
 
 export function useGlobalVariableSchema(globalVariableId?: number) {
-  const { t } = useI18n()
+	const { t } = useI18n()
 
-  const globalVariableSchema = z.object({
-    name: z.string(t('validation.global_variables.name.required'))
-      .normalize()
-      .min(3, t('validation.global_variables.name.min'))
-      .max(120, t('validation.global_variables.name.max'))
-      .refine(async (value) => {
-        const condition = globalVariableId
-          ? and(eq(global_variables.name, value), ne(global_variables.id, globalVariableId))!
-          : eq(global_variables.name, value)
-        const exist = await countWhere(global_variables, condition)
-        return exist <= 0
-      }, t('validation.global_variables.name.not_unique')
-      ),
-    value: z.string(t('validation.global_variables.value.required')),
-    is_secret: z.boolean().default(false),
-    description: z.string().max(1000, t('validation.global_variables.description.max')).optional(),
-  })
+	const slugRegex = /^[a-z0-9]+(?:_[a-z0-9]+)*$/
 
-  return {
-    globalVariableSchema,
-  }
+	const globalVariableSchema = z.object({
+		name: z
+			.string(t('validation.global_variables.name.required'))
+			.normalize()
+			.min(3, t('validation.global_variables.name.min'))
+			.max(120, t('validation.global_variables.name.max')),
+		slug: z
+			.string(t('validation.global_variables.slug.required'))
+			.min(3, t('validation.global_variables.slug.min'))
+			.max(120, t('validation.global_variables.slug.max'))
+			.regex(slugRegex, t('validation.global_variables.slug.invalid'))
+			.refine(async value => {
+				const condition = globalVariableId
+					? and(eq(global_variables.slug, value), ne(global_variables.id, globalVariableId))!
+					: eq(global_variables.slug, value)
+				const exist = await countWhere(global_variables, condition)
+				return exist <= 0
+			}, t('validation.global_variables.slug.not_unique')),
+		value: z.string(t('validation.global_variables.value.required')),
+		is_secret: z.boolean().default(false),
+		description: z.string().max(1000, t('validation.global_variables.description.max')).optional(),
+	})
+
+	return {
+		globalVariableSchema,
+	}
 }
 
 export type GlobalVariableSchema = z.output<ReturnType<typeof useGlobalVariableSchema>['globalVariableSchema']>
