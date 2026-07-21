7ef9a4701681e181199a1f8d99740dc22cd95158
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2026-07-21 16:41:20 +0200

    feat: página de error (no encontrado)

---
+++ src/pages/dashboard/docker_composes/[...path].vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { useI18n } from 'vue-i18n'
+
+import { getModuleIcon } from '@/utils/icons'
+
+const { t } = useI18n()
+</script>
+
+<template>
+	<UError
+		:icon="getModuleIcon('docker_composes', 'off')"
+		:clear="false"
+		:error="{
+			statusCode: 404,
+			statusMessage: t('components.error.docker_compose.statusMessage'),
+			message: t('components.error.docker_compose.message'),
+		}"
+		:ui="{ root: 'h-full min-h-full' }"
+	/>
+</template>
