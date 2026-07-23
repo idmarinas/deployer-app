ec4d8292feb4c2badaab0661c29b631e3841e086
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2026-07-23 14:03:18 +0200

    chore: botones reutilizables para los mismos casos

---
+++ src/composables/useToolbarButtons.ts
@@ -0,0 +1,120 @@
+import type { CommandResponse } from '@/types/tauri-types'
+import type { Ref, ShallowRef } from 'vue'
+import type { PositionedButton } from './usePositionedButtons'
+
+import { usePositionedButtons } from './usePositionedButtons'
+
+import { useI18n } from 'vue-i18n'
+import { useRouter } from 'vue-router'
+
+import { useConfirmDialog } from '@/composables/useDialog'
+import useToaster from '@/composables/useToaster'
+import { ICONS } from '@/utils/icons'
+
+import { invoke } from '@tauri-apps/api/core'
+
+export function useToolbarButtons(
+	moduleName: string,
+	item: ShallowRef<{ name: string; id: number }>,
+	loading: Ref<boolean>,
+) {
+	const router = useRouter()
+	const toaster = useToaster()
+	const { t } = useI18n()
+	const { resolveButtons } = usePositionedButtons()
+
+	function useViewButtons(buttons: PositionedButton[] = [], tooltip: boolean = false) {
+		const defaultBtns: PositionedButton[] = [
+			{
+				id: 'edit',
+				icon: ICONS.actions.edit,
+				color: 'info',
+				variant: 'outline',
+				label: t('common.actions.edit'),
+				loading: loading.value,
+				tooltip,
+				onClick() {
+					router.push({
+						name: `dashboard-${moduleName}-id-edit` as any,
+						params: { id: item.value.id },
+					})
+				},
+			},
+			{
+				id: 'delete',
+				icon: ICONS.actions.delete,
+				label: t('common.actions.delete'),
+				color: 'error',
+				loading: loading.value,
+				async onClick() {
+					const dialog = useConfirmDialog()
+
+					const result = await dialog({
+						title: t('common.confirm.delete.label'),
+						description: t('common.confirm.delete.description', { name: item.value.name }),
+						type: 'cancel_delete',
+					})
+
+					if (result) {
+						const notice = toaster.warning(
+							t('notifications.projects.delete.loading.title'),
+							t('notifications.projects.delete.loading.description', { name: item.value.name }),
+							{ duration: 0 },
+						)
+
+						const result = await invoke<CommandResponse>('crud_delete_project', { id: item.value.id })
+
+						if (result.success) {
+							toaster.toast.update(
+								notice.id,
+								toaster.success(
+									t('notifications.projects.delete.success.title'),
+									t('notifications.projects.delete.success.description', { name: item.value.name }),
+									{ id: notice.id, duration: undefined },
+								),
+							)
+						} else {
+							toaster.toast.update(
+								notice.id,
+								toaster.error(
+									t('notifications.projects.delete.error.title'),
+									t('notifications.projects.delete.error.description', { name: item.value.name }),
+									{ id: notice.id, duration: undefined },
+								),
+							)
+						}
+
+						await router.push({ name: `dashboard-${moduleName}` as any })
+					}
+				},
+				tooltip,
+			},
+			{
+				id: 'list',
+				icon: 'i-tabler-list',
+				label: t('common.back.list'),
+				loading: loading.value,
+				tooltip,
+				async onClick() {
+					router.push({ name: `dashboard-${moduleName}` } as any)
+				},
+			},
+			{
+				id: 'cancel',
+				icon: ICONS.actions.cancel,
+				label: t('common.actions.cancel'),
+				variant: 'outline',
+				color: 'neutral',
+				loading: loading.value,
+				tooltip,
+				onClick: () => router.back(),
+			},
+		]
+
+		return resolveButtons(defaultBtns, buttons)
+	}
+
+	return {
+		useViewButtons,
+	}
+}
