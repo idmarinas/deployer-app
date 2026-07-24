ccdb70b4fdf41db1fff031e6e77a7d8d3c67b90e
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2026-07-24 13:53:23 +0200

    feat: se agrega botón para agregar un nuevo passkey

--- src/components/form/inputs/selects/SelectKeypass.vue
+++ src/components/form/inputs/selects/SelectKeypass.vue
@@ -2,20 +2,30 @@
 import type { SelectMenuItem } from '@nuxt/ui'
 
 import { useI18n } from 'vue-i18n'
+import { useRouter } from 'vue-router'
+
 import { useSelectPasskeys } from '@/loaders/passkeys'
+import { ICONS } from '@/utils/icons'
 </script>
 
 <script setup lang="ts">
 const { t } = useI18n()
+const router = useRouter()
 const { data: items, isLoading } = useSelectPasskeys()
 </script>
 
 <template>
-  <USelectMenu
-    value-key="id"
-    :items="items as SelectMenuItem[]"
-    :loading="isLoading"
-    :disabled="isLoading"
-    :placeholder="t('form.shared.placeholder.passkeys.select')"
-  />
+	<UFieldGroup>
+		<USelectMenu
+			value-key="id"
+			:items="items as SelectMenuItem[]"
+			:loading="isLoading"
+			:disabled="isLoading"
+			:placeholder="t('form.shared.placeholder.passkeys.select')"
+			class="w-full"
+		/>
+		<UTooltip :text="t('form.passkeys.title.add')" :delay-duration="0">
+			<UButton :icon="ICONS.actions.add" @click="router.push({ name: 'dashboard-passkeys-add' })" />
+		</UTooltip>
+	</UFieldGroup>
 </template>
