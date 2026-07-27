2b9c2d622be1ffe77db19a46a4cdd86196a93ad7
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2026-07-27 15:22:49 +0200

    chore: componente para ver información de un item en la página de vista

---
+++ src/components/pages/ViewCard.vue
@@ -0,0 +1,56 @@
+<!-- Componente usando en las páginas de vistas (view).vue -->
+
+<script lang="ts">
+import { useI18n } from 'vue-i18n'
+</script>
+
+<script setup lang="ts">
+const props = defineProps<{
+	id: number
+	enabled: boolean
+	description?: object
+	created_at: string
+	updated_at: string
+	updateCommand: string
+	onUpdatedEnabled: (enabled: boolean) => void
+}>()
+
+const { t, locale } = useI18n()
+</script>
+
+<template>
+	<UCard :ui="{ title: 'flex justify-between' }">
+		<template #title>
+			<div class="flex items-center gap-2">
+				<UBadge color="neutral" variant="soft"> ID: {{ id }} </UBadge>
+				<ToggleEnabled :enabled="enabled" :command="updateCommand" :id="id" @updated="onUpdatedEnabled" />
+			</div>
+			<div class="flex items-center gap-2">
+				<slot name="title-right" />
+			</div>
+		</template>
+
+		<template #description>
+			<DescriptionViewer :value="description" :placeholder="t('common.empty.description')" />
+		</template>
+
+		<template #default>
+			<slot />
+		</template>
+
+		<template #footer>
+			<div class="flex gap-4 items-center justify-between text-xs text-muted">
+				<span class="flex gap-1.5 items-center">
+					<UIcon name="i-tabler-calendar-plus" class="size-4" />
+					<strong>{{ t('entity.common.created_at') }}:</strong>
+					{{ new Date(created_at).toLocaleString(locale, { dateStyle: 'long', timeStyle: 'short' }) }}
+				</span>
+				<span class="flex gap-1.5 items-center">
+					<UIcon name="i-tabler-calendar-time" class="size-4" />
+					<strong>{{ t('entity.common.updated_at') }}:</strong>
+					{{ new Date(updated_at).toLocaleString(locale, { dateStyle: 'long', timeStyle: 'short' }) }}
+				</span>
+			</div>
+		</template>
+	</UCard>
+</template>
