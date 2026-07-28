0b06710ce65c30cc28446b6924739caf9e6d32f5
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2026-07-28 19:17:47 +0200

    chore: se agrega el type cancel_update

--- src/composables/useDialog.ts
+++ src/composables/useDialog.ts
@@ -1,50 +1,50 @@
 import type { GeneratedPasskey, Passkey } from '@/types/tauri-types'
 
 import ConfirmDialog from '@/components/overlay/ConfirmDialog.vue'
-import GeneratedPasskeyFormDialog from '@/components/overlay/forms/GeneratePasskeyDialog.vue'
 import CopyPasskeyToServerDialog from '@/components/overlay/forms/CopyPasskeyToServerDialog.vue'
+import GeneratedPasskeyFormDialog from '@/components/overlay/forms/GeneratePasskeyDialog.vue'
 
 export interface ConfirmDialogOptions {
-  title: string
-  description?: string,
-  type: 'yes_no' | 'cancel_confirm' | 'cancel_delete'
+	title: string
+	description?: string
+	type: 'yes_no' | 'cancel_confirm' | 'cancel_delete' | 'cancel_update'
 }
 
 export const useConfirmDialog = () => {
-  const overlay = useOverlay()
+	const overlay = useOverlay()
 
-  return (options: ConfirmDialogOptions): Promise<boolean> => {
-    const modal = overlay.create(ConfirmDialog, {
-      destroyOnClose: true,
-      props: options
-    })
+	return (options: ConfirmDialogOptions): Promise<boolean> => {
+		const modal = overlay.create(ConfirmDialog, {
+			destroyOnClose: true,
+			props: options,
+		})
 
-    return modal.open()
-  }
+		return modal.open()
+	}
 }
 
 export const useGeneratePasskeyDialog = () => {
-  const overlay = useOverlay()
+	const overlay = useOverlay()
 
-  return (options: Record<string, any> = {}): Promise<GeneratedPasskey | false> => {
-    const modal = overlay.create(GeneratedPasskeyFormDialog, {
-      destroyOnClose: true,
-      props: options
-    })
+	return (options: Record<string, any> = {}): Promise<GeneratedPasskey | false> => {
+		const modal = overlay.create(GeneratedPasskeyFormDialog, {
+			destroyOnClose: true,
+			props: options,
+		})
 
-    return modal.open()
-  }
+		return modal.open()
+	}
 }
 
 export const useCopyPasskeyToServer = () => {
-  const overlay = useOverlay()
+	const overlay = useOverlay()
 
-  return (options: { passkey: Passkey }): Promise<boolean> => {
-    const modal = overlay.create(CopyPasskeyToServerDialog, {
-      destroyOnClose: true,
-      props: options
-    })
+	return (options: { passkey: Passkey }): Promise<boolean> => {
+		const modal = overlay.create(CopyPasskeyToServerDialog, {
+			destroyOnClose: true,
+			props: options,
+		})
 
-    return modal.open()
-  }
+		return modal.open()
+	}
 }
