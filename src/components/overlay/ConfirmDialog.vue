da9992cfe4d63b3d1d6094d1c20806d1d6738933
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2026-07-28 19:16:42 +0200

    chore: se agregan iconos a los botones

--- src/components/overlay/ConfirmDialog.vue
+++ src/components/overlay/ConfirmDialog.vue
@@ -2,38 +2,56 @@
 import { useI18n } from 'vue-i18n'
 
 import { ConfirmDialogOptions } from '@/composables/useDialog'
-import { computed } from 'vue';
+import { ICONS } from '@/utils/icons'
+import { computed } from 'vue'
 
 const props = defineProps<ConfirmDialogOptions>()
 
 const { t } = useI18n()
 
 const confirmColor = computed(() => {
-  if (props.type === 'cancel_delete') {
-    return 'error'
-  } else if (props.type === 'cancel_confirm') {
-    return 'warning'
-  }
+	if (props.type === 'cancel_delete') {
+		return 'error'
+	} else if (props.type === 'cancel_confirm') {
+		return 'warning'
+	}
 
-  return 'primary'
+	return 'primary'
 })
 
+const confirmIcon = computed(() => {
+	if (props.type === 'cancel_delete') {
+		return ICONS.actions.delete
+	} else if (props.type === 'cancel_confirm') {
+		return ICONS.actions.submit
+	} else if (props.type === 'cancel_update') {
+		return 'i-tabler-download'
+	}
+
+	return undefined
+})
 
 const emits = defineEmits<{
-  close: [value: boolean]
+	close: [value: boolean]
 }>()
 </script>
 
 <template>
-  <UModal
-    :title="title"
-    :description="description"
-    :dismissible="false"
-    :ui="{ footer: 'justify-end' }"
-  >
-    <template #footer>
-      <UButton :label="t(`overlays.dialog.${type}.cancel`)" color="neutral" variant="outline" @click="emits('close', false)" />
-      <UButton :label="t(`overlays.dialog.${type}.confirm`)" :color="confirmColor" @click="emits('close', true)" />
-    </template>
-  </UModal>
+	<UModal :title="title" :description="description" :dismissible="false" :ui="{ footer: 'justify-end' }">
+		<template #footer>
+			<UButton
+				:label="t(`overlays.dialog.${type}.cancel`)"
+				color="neutral"
+				variant="outline"
+				icon="i-tabler-cancel"
+				@click="emits('close', false)"
+			/>
+			<UButton
+				:label="t(`overlays.dialog.${type}.confirm`)"
+				:color="confirmColor"
+				:icon="confirmIcon"
+				@click="emits('close', true)"
+			/>
+		</template>
+	</UModal>
 </template>
