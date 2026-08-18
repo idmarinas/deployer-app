feca5a223f8c60ebc6a747e1a3acd30d1f091a32
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2026-08-18 20:11:09 +0200

    reformat: formateado

--- src/pages/deployer.vue
+++ src/pages/deployer.vue
@@ -1,12 +1,12 @@
 <!-- Layout básico centrado (vertical/horizontal) -->
 
 <template>
-    <UMain>
-        <div class="h-screen flex items-center justify-center px-4">
-            <UColorModeSelect class="absolute top-8 left-8 z-10" />
-            <LocaleChange class="absolute top-8 right-8 z-10" />
+	<UMain>
+		<div class="h-screen flex items-center justify-center px-4">
+			<UColorModeSelect class="absolute top-8 left-8 z-10" />
+			<LocaleChange class="absolute top-8 right-8 z-10" />
 
-            <RouterView />
-        </div>
-    </UMain>
+			<RouterView />
+		</div>
+	</UMain>
 </template>
