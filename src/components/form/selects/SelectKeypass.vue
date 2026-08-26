6e8a0928891c5093f314f30cf19f56cfe036dde4
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2026-08-26 21:01:13 +0200

    chore: se permite ignorar passkey y ver solo los habilitados

--- src/components/form/selects/SelectKeypass.vue
+++ src/components/form/selects/SelectKeypass.vue
@@ -1,19 +1,55 @@
 <script lang="ts">
 import type { SelectMenuItem } from '@nuxt/ui'
 
+import { computed, watch } from 'vue'
 import { useI18n } from 'vue-i18n'
 import { useRouter } from 'vue-router'
 
 import { useSelectPasskeys } from '@/loaders/passkeys'
 import { ICONS } from '@/utils/icons'
+
+type SelectMenuItemExtends = SelectMenuItem & {
+	id: number
+	enabled: boolean
+	key_type: string
+}
 </script>
 
 <script setup lang="ts">
-const state = defineModel<number | null>()
+const state = defineModel<number | undefined>()
+const props = withDefaults(
+	defineProps<{
+		ignore?: Set<number>
+		onlyEnabled?: boolean
+	}>(),
+	{
+		onlyEnabled: true,
+	},
+)
 
 const { t } = useI18n()
 const router = useRouter()
 const { data: items, isLoading } = useSelectPasskeys()
+
+const availableItems = computed(() => {
+	const ignore = props.ignore || new Set([])
+	const itemsFiltered = ((items.value ?? []) as any[]).filter(item => !ignore.has(item.id))
+
+	if (props.onlyEnabled) {
+		return itemsFiltered.filter(item => item.enabled === true)
+	}
+
+	return itemsFiltered
+})
+
+watch(
+	() => props.onlyEnabled,
+	() => {
+		if (!availableItems.value.find(item => item.id === state.value)) {
+			state.value = undefined
+		}
+	},
+)
 </script>
 
 <template>
@@ -22,11 +58,24 @@ const { data: items, isLoading } = useSelectPasskeys()
 			v-model="state"
 			value-key="id"
 			class="flex-1"
-			:items="items as SelectMenuItem[]"
+			:items="availableItems as SelectMenuItemExtends[]"
 			:loading="isLoading"
 			:disabled="isLoading"
 			:placeholder="t('form.shared.placeholder.passkeys.select')"
-		/>
+			:icon="ICONS.auth.key"
+		>
+			<template #item-leading="{ item }">
+				<UBadge
+					:color="item.enabled ? 'success' : 'error'"
+					variant="outline"
+					:icon="item.enabled ? ICONS.auth.key : ICONS.auth.keyOff"
+					size="sm"
+				/>
+			</template>
+			<template #item-trailing="{ item }">
+				<UBadge color="neutral" variant="outline" icon="i-tabler-square-key" size="sm" :label="item.key_type" />
+			</template>
+		</USelectMenu>
 		<UTooltip :text="t('form.passkeys.title.add')" :delay-duration="0">
 			<UButton
 				:loading="isLoading"
