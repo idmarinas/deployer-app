6151911146876134c6592e9a76c9e83e20d735c1
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2026-09-09 13:54:14 +0200

    chore: no forma parte de la versión 0.1.0

--- src/locales/es/components/projectsMenu.ts
+++
@@ -1,7 +0,0 @@
-import type { LocaleMessageValue } from 'vue-i18n'
-
-export default {
-	select: 'Seleccionar proyecto',
-	create: 'Crear proyecto',
-	manage: 'Gestionar proyectos',
-} satisfies LocaleMessageValue
