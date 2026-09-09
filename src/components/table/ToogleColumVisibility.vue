45ee7defde313c0a679c0d92b1956c2ea81a3485
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2026-09-09 13:57:53 +0200

    chore: se actualizan las claves de traducción

--- src/components/table/ToogleColumVisibility.vue
+++ src/components/table/ToogleColumVisibility.vue
@@ -6,33 +6,34 @@ import { useI18n } from 'vue-i18n'
 <script setup lang="ts">
 const props = defineProps<{ tableApi: Table<any> | undefined }>()
 
-const { t } = useI18n()</script>
+const { t } = useI18n()
+</script>
 
 <template>
-<UDropdownMenu
-  :items="
-    tableApi
-      ?.getAllColumns()
-      .filter((column: Column<any>) => column.getCanHide())
-      .map((column: Column<any>) => ({
-        label: column.columnDef.header as string,
-        type: 'checkbox' as const,
-        checked: column.getIsVisible(),
-        onUpdateChecked(checked: boolean) {
-          tableApi?.getColumn(column.id)?.toggleVisibility(!!checked)
-        },
-        onSelect(e: Event) {
-          e.preventDefault()
-        }
-      }))
-  "
-  :content="{ align: 'end' }"
->
-  <UButton
-    :label="t('components.table.columns')"
-    color="neutral"
-    variant="outline"
-    trailing-icon="i-tabler-chevron-down"
-  />
-</UDropdownMenu>
+	<UDropdownMenu
+		:items="
+			tableApi
+				?.getAllColumns()
+				.filter((column: Column<any>) => column.getCanHide())
+				.map((column: Column<any>) => ({
+					label: column.columnDef.header as string,
+					type: 'checkbox' as const,
+					checked: column.getIsVisible(),
+					onUpdateChecked(checked: boolean) {
+						tableApi?.getColumn(column.id)?.toggleVisibility(!!checked)
+					},
+					onSelect(e: Event) {
+						e.preventDefault()
+					},
+				}))
+		"
+		:content="{ align: 'end' }"
+	>
+		<UButton
+			:label="t('common.table.columns')"
+			color="neutral"
+			variant="outline"
+			trailing-icon="i-tabler-chevron-down"
+		/>
+	</UDropdownMenu>
 </template>
